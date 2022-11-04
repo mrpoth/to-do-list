9ca7f8dc65d438ee4071d8e13ccbf2415b42fa89
Joel Darlington <Joel.Darlington@mlp.agency>
2022-11-04 17:24:16 +0000

    Adding resources to tasks layout

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -9,6 +9,8 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
